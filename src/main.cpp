9de370a0149666accf9c841fabe6a71fd4c4caac
Hendrik Roch <hroch@physik.uni-bielefeld.de>
2024-08-02 19:27:43 -0400

    Add all missing files, no implementation yet

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,10 @@
 #include "GeneralGeometryElement.h"
 #include "Line.h"
 #include "Polygon.h"
+#include "Polyhedron.h"
+#include "Square.h"
+#include "Cube.h"
+#include "Hypercube.h"
+#include "Cornelius.h"
 
 int main(int argc, char const *argv[]) { return 0; }
